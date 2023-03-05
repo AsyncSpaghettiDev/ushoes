2afa5a597bd5b091bf2f86f80415f59bac976023
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2023-03-04 20:49:48 -0500

    started creating shoeDetail page

--- src/pages/ShoeDetail.tsx
+++ src/pages/ShoeDetail.tsx
@@ -1,9 +1,37 @@
+import { Box, Button, Flex, VStack } from "@chakra-ui/react";
 
 
 export default function ShoeDetail() {
     return (
         <>
-        
+        <Box mt='5' w='100%' display='flex' flexDirection='row'>
+            <Box backgroundColor='lightpink' w='60%' h='40vh'>
+
+            </Box>
+
+            <Box backgroundColor='lightblue' w='40%' h='40vh'>
+                <VStack>
+                    <Box>
+                        <Box>
+                            Sneaker 1
+                        </Box>
+                        <Box>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores rem sequi tenetur suscipit quos?
+                        </Box>
+                        <Box>
+                            $165
+                        </Box>
+                    </Box>
+                    <Box w='90%' h='20vh' outline='1px solid gray'>
+                        <Flex>
+                            <Box>
+                                <Button>4</Button>
+                            </Box>
+                        </Flex>
+                    </Box>
+                </VStack>
+            </Box>
+        </Box>
         </>
     )
 }
