4bfc6c0ea67d45d329842b593ee0599e8c420171
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2023-03-08 21:53:07 -0500

    minor changes

--- backend/untiedshoes/settings.py
+++ backend/untiedshoes/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'untiedshoes.eba-fmqqvsv5.us-east-1.elasticbeanstalk.com'
+    ]
 
 
 # Application definition
